f511132a83aaa09901792b3d7e5fa05d43f401b8
gautambitious <mvgt.gautam@gmail.com>
2020-10-01 13:23:28 +0530

    Web Upload

---
+++ material-kit-master/changes.js
@@ -0,0 +1,8 @@
+Dropzone.options.myAwesomeDropzone = {
+  paramName: "file", // The name that will be used to transfer the file
+  maxFilesize: 5, // MB
+  uploadMultiple: false,
+  maxFiles: 1,
+  acceptedFiles: "image/*",
+  dictDefaultMessage: "Drop Images Here To Enhance!",
+};
